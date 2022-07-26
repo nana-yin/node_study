cb7073d4102bc6b4ef6b3b0e2fbebb57cfcf1462
yinna <yn@bvrft.com>
2022-07-26 18:08:35 +0800

    feat: 学习请求的两种方式

--- 06_express/module/08_route.js
+++ 06_express/module/08_route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+// get 请求
 router.get("/list", (req,res) => {
   const query =  req.query; // 通过req.query来获取客户端发送过来的查询字符串
   res.send({
@@ -10,12 +11,21 @@ router.get("/list", (req,res) => {
   })
 })
 
+// post 请求
 router.post("/detail", (req,res) => {
   const bd = req.body; // 通过req.body来获取请求体包含的 url-encoded 格式的数据
   res.send({
     status: 0, // 状态，0 表示成功  1表示失败
     msg: "POST请求成功",
     data: bd
+  })
+})
+
+// delete 请求
+router.delete("/delete", (req,res) => {
+  res.send({
+    status: 0, // 状态，0 表示成功  1表示失败
+    msg: "Delete请求成功",
   })
 })
 
--- 06_express/src/08_使用express写接口.js
+++ 06_express/src/08_使用express写接口.js
@@ -2,8 +2,47 @@ const express  = require("express");
 const app = express();
 // 解决跨域：npm i cors
 const cors = require("cors");
-// 配置中间件
+// 配置cors
 app.use(cors())
+
+/**
+ * cors响应头：
+ *    1、Access-Control-Allow-Origin：
+ *        语法格式：Access-Control-Allow-Origin: <origin> | *
+ *          origin：指定允许访问该资源的外域url。
+ *          * ：任何域的请求。
+ *    2、Access-Control-Allow-Headers：
+ *       默认情况下，cors仅支持客户端向服务器发送如下9个请求头：
+ *        Accept
+ *        Accept-Language
+ *        Content-Language
+ *        DPR
+ *        DownLink
+ *        Save-Data
+ *        Viewport-Width
+ *        width
+ *        Content-Type（只有三个值：application/x-www-form-urlencoded、multipart/form-data、text/plain）
+ *     如果要设置额外的请求头，则需要使用 Access-Control-Allow-Headers 进行声明。
+ *     例如：允许客户端额外向服务器发送 Content-Type 请求头和 X-Custom-Header 请求头。
+ *         // 注意：多个请求头之间用逗号进行分隔。
+ *         res.setHeader('Access-Control-Allow-Header', "Content-type, X-Custom-Header") 
+ *     3、Access-Control-Allow-Methods：
+ *        默认情况下，cors仅支持客户端发起 GET、POST、HEAD请求。
+ *        如果客户端希望通过PUT、DELETE 等方式请求服务器的资源，则需要使用 Access-Control-Allow-Methods。
+ *      例1：只允许 POST、GET、DELETE、HEAD 请求方法
+ *         res.setHeader('Access-Control-Allow-Methods', 'POST、GET、DELETE、HEAD')
+ *      例2：允许所有的 HTTP 请求方法
+ *         res.setHeader('Access-Control-Allow-Methods', '*')
+ *     4、简单请求（条件要同时满足）：
+ *        ① 请求方式：GET、POST、HEAD 三者之一
+ *        ② Http头部信息没有自定义的头部字段（即只有9个默认的请求头）
+ *     5、预检请求（只满足其中的一个条件即可）：
+ *        ① 请求方式：GET、POST、HEAD 之外的请求方法类型
+ *        ② 请求头中包含自定义头部字段
+ *        ③ 向服务器中发送了 application/json 格式的数据
+ */
+
+
 // 引用解析url-encoded的中间件
 app.use(express.urlencoded({extended: false}))
 
